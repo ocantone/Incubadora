e93c666a8538a4c8e4bb4da69c3bf478d8b0387e
Osvaldo Cantone <ocantone@gmail.com>
2021-07-05 00:25:36 -0300

    Edición de los comentarios

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
-
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+/**
+INCUBADORA TATO V1.0
+USa las siguientes bibliotecas Arduino:
+- DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+- Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+**/
 
 #include <Arduino.h>
 #include <LiquidCrystal_PCF8574.h>
